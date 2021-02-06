101ed71496825e6417ed927148ce973bcc5801ba
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-02-06 13:32:37 -0500

    updated copyright info, added jQuery link to setup function for contacting me once user submits contact request

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var emailBtn = $("#submit");
+
+    // Write a function that sends email to me based on submitted comment and contact information in the contact section
+
+    emailBtn.click(function() {
+        console.log("hello");
+    })
+
+})
