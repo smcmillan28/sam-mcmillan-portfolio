07a0f9f55b0de4632ffc392999a427187edb2e92
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-02-09 15:31:09 -0500

    added a few images to profile section and click function to allow user to cycle through photos

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var emailBtn = $("#submit");
+    var profImage = $("#marathon");
+
+    // Creating an array with all image paths for profile picture options
+    var profPics = ["./assets/images/snowpoc.JPG",
+        "./assets/images/disney.jpg",
+        "./assets/images/hole-in-one.jpg",
+        "./assets/images/knox.jpg",
+        "./assets/images/murder-mystery.jpg",
+        "./assets/images/skiing.jpg",
+        "./assets/images/stones.JPG",
+        "./assets/images/tedeschi.jpg",
+        "./assets/images/windy-city.jpg",
+        "./assets/images/about-me.JPG"]; 
 
     // Write a function that sends email to me based on submitted comment and contact information in the contact section
-
-    emailBtn.click(function() {
+    emailBtn.click(function () {
         console.log("hello");
-    })
+    });
+
+    profImage.click(function () {
+        var randNum = Math.floor(Math.random() * 10);
+        profImage.attr("src", profPics[randNum]);
+    });
 
 })
